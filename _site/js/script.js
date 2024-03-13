ce3ca4cf92a5d9979da1202274ca74c7ac912462
isiltama <isiltama@confederationcollege.ca>
2024-03-13 12:04:36 -0400

    total refactor

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,38 +1,58 @@
-console.log('we have js!');
+let moviePosterIndex = 0;
+let tvPosterIndex = 0;
 
-// server.js
+function updatePoster() {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1; // Months are zero-based
+  const year = currentDate.getFullYear();
+  const dateString = year + "-" + month + "-" + day;
 
-const express = require('express');
-const nunjucks = require('nunjucks');
-const path = require('path');
-const fs = require('fs');
+  const moviePosters = [
+    "moviePosters/1917_film_poster.jpg",
+    "moviePosters/Guardians_of_the_Galaxy_Vol._3_poster.jpg",
+    "moviePosters/LOTR.jpg",
+    "moviePosters/Elvis_2022_poster.jpg",
+    "moviePosters/Avengers_Endgame_poster.jpg",
+    "moviePosters/Bohemian_Rhapsody_poster.png",
+    "moviePosters/Rocketman_(film).png",
+    "moviePosters/Walk_the_line_poster.jpg",
+    "moviePosters/Wonder_Woman_(2017_film)_poster.jpg",
+    "moviePosters/A_Star_is_Born.png"
+  ];
 
-const app = express();
+  const tvShowPosters = [
+    "tvShows/Band_of_Brothers_poster.jpg",
+    "tvShows/blacksails.jpg",
+    "tvShows/Gravity_Falls,_Vol._1_cover_art.jpg",
+    "tvShows/merlin_bbc.jpg",
+    "tvShows/Our_Flag_Means_Death_Season_1_Poster.jpg",
+    "tvShows/Narcos_season_1.png",
+    "tvShows/resDogs.jpg",
+    "tvShows/Stranger_Things_season_4.jpg",
+    "tvShows/The_Last_of_Us_season_1_Blu-ray.png",
+    "tvShows/The_Mandalorian_season_2_poster.jpg"
+  ];
 
-// Set up Nunjucks environment
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-});
+  if (day !== moviePosterIndex) {
+    moviePosterIndex = day;
+    const randomMovieIndex = day % moviePosters.length;
+    document.getElementById("movie-poster-image").src = moviePosters[randomMovieIndex];
+    document.getElementById("movie-poster-title").innerText = "Movie of the Day - " + dateString;
+  }
 
-// Function to get a random file from a directory
-function getRandomFileFromDirectory(directory) {
-    const files = fs.readdirSync(directory);
-    const randomFile = files[Math.floor(Math.random() * files.length)];
-    return randomFile;
+  if (day !== tvPosterIndex) {
+    tvPosterIndex = day;
+    const randomTvIndex = day % tvShowPosters.length;
+    document.getElementById("tv-poster-image").src = tvShowPosters[randomTvIndex];
+    document.getElementById("tv-poster-title").innerText = "TV Show of the Day - " + dateString;
+  }
 }
 
-// Serve static files from moviePosters and tvShows directories
-app.use('/moviePosters', express.static(path.join(__dirname, 'moviePosters')));
-app.use('/tvShows', express.static(path.join(__dirname, 'tvShows')));
+// Call the function to initially set the poster
+updatePoster();
 
-// Route to render the home page
-app.get('/', (req, res) => {
-    const movieImagePath = getRandomFileFromDirectory(path.join(__dirname, 'moviePosters'));
-    const tvShowImagePath = getRandomFileFromDirectory(path.join(__dirname, 'tvShows'));
-    res.render('index.njk', { movieImagePath, tvShowImagePath });
-});
+// Call the function every 24 hours to update the poster
+setInterval(updatePoster, 24 * 60 * 60 * 1000);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+  
